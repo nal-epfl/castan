e50d7a252bad7b21db706cdda094a7c137bacf35
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2016-11-23 20:48:01 +0100

    Added debug build to path.

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export PATH=$PATH:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Release+Debug+Asserts/bin
+export PATH=$PATH:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Debug+Asserts/bin:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Release+Debug+Asserts/bin
