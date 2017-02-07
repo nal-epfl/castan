9a7257a290f814cdc2decc76178c714bc8b7e724
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-02-07 21:01:46 +0100

    Use generic calls to memory model and have CASTAN replace them with the specific ones. Try making different parts of the input symbolic.

--- examples/data-structures/lpm-main.c
+++ examples/data-structures/lpm-main.c
@@ -9,47 +9,56 @@
 
 #include <klee/klee.h>
 
-void memory_model_generic_start();
-void memory_model_generic_dump();
-void memory_model_generic_stop();
+void memory_model_start();
+void memory_model_dump();
+void memory_model_stop();
 
 int main(int argc, char *argv[]) {
   init_prefix_db();
 
-  struct in_addr ip;
-  int prefix_len;
-  data_t data;
+  struct in_addr set_ip;
+  int set_prefix_len;
+  data_t set_data;
 
 #ifdef __clang__
-  klee_make_symbolic((void*)&ip, sizeof(ip), "ip");
+//   klee_make_symbolic((void*)&set_ip, sizeof(set_ip), "set_ip");
+  inet_pton(AF_INET, "127.0.0.1", &set_ip);
 
-  klee_make_symbolic((void*)&prefix_len, sizeof(prefix_len), "prefix_len");
-  klee_assume(prefix_len >= 0);
-  klee_assume(prefix_len <= 24);
+  klee_make_symbolic((void*)&set_prefix_len, sizeof(set_prefix_len), "set_prefix_len");
+  klee_assume(set_prefix_len >= 0);
+  klee_assume(set_prefix_len <= 24);
+//   set_prefix_len = 24;
 
-  klee_make_symbolic((void*)&data, sizeof(data), "data");
+  klee_make_symbolic((void*)&set_data, sizeof(set_data), "set_data");
+//   set_data = 1;
 #else
-  inet_pton(AF_INET, "127.0.0.1", &ip);
-  prefix_len = 24;
-  data = 1;
+#endif
+
+// #ifdef __clang__
+//   memory_model_start();
+//   memory_model_dump();
+// #endif
+
+  set_prefix_data(&set_ip, set_prefix_len, set_data);
+
+  struct in_addr get_ip;
+
+#ifdef __clang__
+  klee_make_symbolic((void*)&get_ip, sizeof(get_ip), "get_ip");
+#else
+  inet_pton(AF_INET, "127.0.0.1", &get_ip);
 #endif
 
 #ifdef __clang__
-  memory_model_generic_start();
-  memory_model_generic_dump();
+  memory_model_start();
+//   memory_model_dump();
 #endif
 
-  set_prefix_data(&ip, prefix_len, data);
+  get_ip_data(&get_ip);
 
 #ifdef __clang__
-  memory_model_generic_dump();
-#endif
-
-  get_ip_data(&ip);
-
-#ifdef __clang__
-  memory_model_generic_dump();
-  memory_model_generic_stop();
+//   memory_model_dump();
+  memory_model_stop();
 #endif
 
   return 0;
