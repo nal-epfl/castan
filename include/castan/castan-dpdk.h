ee2fe9fb4de549bc140ff459086474d4fff7ae81
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-05 20:45:48 +0200

    Fixed multiply defined symbols.

--- include/castan/castan-dpdk.h
+++ include/castan/castan-dpdk.h
@@ -18,22 +18,23 @@ struct packet {
   };
 };
 
-struct rte_eth_dev rte_eth_devices[RTE_MAX_ETHPORTS];
+struct rte_eth_dev __attribute__((weak)) rte_eth_devices[RTE_MAX_ETHPORTS];
 
-struct rte_mempool_ops_table rte_mempool_ops_table = {
-    .sl = RTE_SPINLOCK_INITIALIZER, .num_ops = 0};
+struct rte_mempool_ops_table __attribute__((weak))
+rte_mempool_ops_table = {.sl = RTE_SPINLOCK_INITIALIZER, .num_ops = 0};
 
 __thread unsigned int __attribute__((weak)) per_lcore__lcore_id = 0;
 
 int rte_eal_tailqs_init(void);
-int rte_eal_init(int argc, char **argv) {
+int __attribute__((weak)) rte_eal_init(int argc, char **argv) {
   if (rte_eal_tailqs_init() < 0)
     rte_panic("Cannot init tail queues for objects\n");
 
   return 0;
 }
 
-int __isoc99_fscanf(FILE *stream, const char *format, ...) {
+int __attribute__((weak))
+__isoc99_fscanf(FILE *stream, const char *format, ...) {
   va_list args;
   va_start(args, format);
   int result = vfprintf(stream, format, args);
@@ -41,7 +42,7 @@ int __isoc99_fscanf(FILE *stream, const char *format, ...) {
   return result;
 }
 
-struct rte_config *rte_eal_get_configuration() {
+struct rte_config __attribute__((weak)) * rte_eal_get_configuration() {
   static struct rte_mem_config mem_config = {
       .mlock.cnt = 0, .qlock.cnt = 0, .mplock.cnt = 0,
   };
@@ -50,12 +51,13 @@ struct rte_config *rte_eal_get_configuration() {
   return &config;
 }
 
-void rte_exit(int exit_code, const char *format, ...) {
+void __attribute__((weak)) rte_exit(int exit_code, const char *format, ...) {
   assert(0);
   exit(exit_code);
 }
 
-void *rte_zmalloc(const char *type, size_t size, unsigned align) {
+void __attribute__((weak)) *
+    rte_zmalloc(const char *type, size_t size, unsigned align) {
   if (align == 0) {
     align = 1;
   }
@@ -65,16 +67,17 @@ void *rte_zmalloc(const char *type, size_t size, unsigned align) {
   return ptr;
 }
 
-void *rte_zmalloc_socket(const char *type, size_t size, unsigned align,
-                         int socket) {
+void __attribute__((weak)) * rte_zmalloc_socket(const char *type, size_t size,
+                                                unsigned align, int socket) {
   return rte_zmalloc(type, size, align);
 }
 
-unsigned rte_socket_id() { return 0; }
+unsigned __attribute__((weak)) rte_socket_id() { return 0; }
 
-uint8_t rte_eth_dev_count() { return 2; }
+uint8_t __attribute__((weak)) rte_eth_dev_count() { return 2; }
 
-void rte_eth_macaddr_get(uint8_t port_id, struct ether_addr *mac_addr) {
+void __attribute__((weak))
+rte_eth_macaddr_get(uint8_t port_id, struct ether_addr *mac_addr) {
   static struct ether_addr addrs[] = {
       {
           0x08, 0x00, 0x27, 0x00, 0x44, 0x71,
@@ -91,11 +94,10 @@ void rte_eth_macaddr_get(uint8_t port_id, struct ether_addr *mac_addr) {
 
 __thread int __attribute__((weak)) per_lcore__rte_errno = 0;
 
-struct rte_mempool *rte_pktmbuf_pool_create(const char *name, unsigned n,
-                                            unsigned cache_size,
-                                            uint16_t priv_size,
-                                            uint16_t data_room_size,
-                                            int socket_id) {
+struct rte_mempool __attribute__((weak)) *
+    rte_pktmbuf_pool_create(const char *name, unsigned n, unsigned cache_size,
+                            uint16_t priv_size, uint16_t data_room_size,
+                            int socket_id) {
   struct rte_mempool *mp = calloc(sizeof(struct rte_mempool), 1);
 
   strncpy(mp->name, name, RTE_MEMPOOL_NAMESIZE);
@@ -104,32 +106,39 @@ struct rte_mempool *rte_pktmbuf_pool_create(const char *name, unsigned n,
   return mp;
 }
 
-int rte_eth_dev_configure(uint8_t port_id, uint16_t nb_rx_queue,
-                          uint16_t nb_tx_queue,
-                          const struct rte_eth_conf *eth_conf) {
+int __attribute__((weak))
+rte_eth_dev_configure(uint8_t port_id, uint16_t nb_rx_queue,
+                      uint16_t nb_tx_queue,
+                      const struct rte_eth_conf *eth_conf) {
   return 0;
 }
 
-int rte_eth_dev_socket_id(uint8_t device) { return 0; }
+int __attribute__((weak)) rte_eth_dev_socket_id(uint8_t device) { return 0; }
 
-enum rte_proc_type_t rte_eal_process_type() { return RTE_PROC_PRIMARY; }
+enum rte_proc_type_t __attribute__((weak)) rte_eal_process_type() {
+  return RTE_PROC_PRIMARY;
+}
 
-int rte_eth_tx_queue_setup(uint8_t port_id, uint16_t tx_queue_id,
-                           uint16_t nb_tx_desc, unsigned int socket_id,
-                           const struct rte_eth_txconf *tx_conf) {
+int __attribute__((weak))
+rte_eth_tx_queue_setup(uint8_t port_id, uint16_t tx_queue_id,
+                       uint16_t nb_tx_desc, unsigned int socket_id,
+                       const struct rte_eth_txconf *tx_conf) {
   return 0;
 }
 
-int rte_eth_rx_queue_setup(uint8_t port_id, uint16_t rx_queue_id,
-                           uint16_t nb_rx_desc, unsigned int socket_id,
-                           const struct rte_eth_rxconf *rx_conf,
-                           struct rte_mempool *mb_pool) {
+int __attribute__((weak))
+rte_eth_rx_queue_setup(uint8_t port_id, uint16_t rx_queue_id,
+                       uint16_t nb_rx_desc, unsigned int socket_id,
+                       const struct rte_eth_rxconf *rx_conf,
+                       struct rte_mempool *mb_pool) {
   return 0;
 }
 
-int rte_eth_dev_start(uint8_t portid) { return 0; }
+int __attribute__((weak)) rte_eth_dev_start(uint8_t portid) { return 0; }
 
-void rte_eth_promiscuous_enable(uint8_t portid) { return; }
+void __attribute__((weak)) rte_eth_promiscuous_enable(uint8_t portid) {
+  return;
+}
 
 #define rte_eth_rx_burst castan_rte_eth_rx_burst
 uint16_t __attribute__((weak))
