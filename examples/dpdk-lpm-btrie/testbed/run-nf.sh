614ed76cafd4be38caa824af45c8276083d92c6f
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-05 20:02:42 +0200

    Added binary trie lpm variant.

---
+++ examples/dpdk-lpm-btrie/testbed/run-nf.sh
@@ -0,0 +1,3 @@
+export RTE_SDK=/home/vagrant/dpdk
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+cd /nf && make && sudo ./build/nf -- --eth-dest 0,08:00:27:53:8b:38 --eth-dest 1,08:00:27:c1:13:47 --pfx2as testbed/routing-table.pfx2as
