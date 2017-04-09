06777e57f016e36a8f01dfefce2f3170adab5eeb
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-09 17:32:06 +0200

    Fix checksums to produce valid pcap files.

--- tools/ktest2pcap/ktest2pcap.cpp
+++ tools/ktest2pcap/ktest2pcap.cpp
@@ -12,7 +12,30 @@
 
 #include "klee/Internal/ADT/KTest.h"
 
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+
 #include <pcap/pcap.h>
+
+uint16_t checksum(const void *buf, size_t len) {
+  unsigned long sum = 0;
+  const uint16_t *pos = (const uint16_t *)buf;
+
+  while (len > 1) {
+    sum += *pos++;
+    if (sum & 0x80000000)
+      sum = (sum & 0xFFFF) + (sum >> 16);
+    len -= 2;
+  }
+
+  while (sum >> 16)
+    sum = (sum & 0xFFFF) + (sum >> 16);
+
+  return ~sum;
+}
 
 int main(int argc, char **argv) {
   assert(argc == 3 && "Usage: ktest2pcap <ktest-file> <pcap-file>");
@@ -33,6 +56,56 @@ int main(int argc, char **argv) {
           .caplen = o->numBytes,
           .len = o->numBytes,
       };
+
+      // Fix checksums.
+      // Check if IP packet.
+      assert(pkthdr.caplen >= sizeof(struct ether_header) &&
+             "Truncated Ethernet frame.");
+      if (ntohs(((struct ether_header *)o->bytes)->ether_type) !=
+          ETHERTYPE_IP) {
+        fprintf(stderr, "Packet with unsupported l3-type: %d\n",
+                ntohs(((struct ether_header *)o->bytes)->ether_type));
+      }
+
+      // Extract IP header.
+      assert(pkthdr.caplen >= sizeof(struct ether_header) + sizeof(struct ip) &&
+             "Truncated IP packet.");
+      struct ip *ip = (struct ip *)(o->bytes + sizeof(struct ether_header));
+      assert(ip->ip_v == 4 && "unsupported IP verion.");
+      assert(ip->ip_hl >= 5 && "Truncated IP packet.");
+
+      ip->ip_sum = 0;
+
+      switch (ip->ip_p) {
+      case 0x06: { // TCP
+        assert(pkthdr.caplen >= sizeof(struct ether_header) + ip->ip_hl * 4 +
+                                    sizeof(struct tcphdr) &&
+               "Truncated TCP segment.");
+        assert(ip->ip_len >= ip->ip_hl * 4 + sizeof(struct tcphdr) &&
+               "Truncated TCP segment.");
+        struct tcphdr *tcp = (struct tcphdr *)(((char *)ip) + ip->ip_hl * 4);
+        assert(tcp->doff >= 5 && "Truncated TCP segment.");
+
+        tcp->check = 0;
+      } break;
+      case 0x11: { // UDP
+        assert(pkthdr.caplen >= sizeof(struct ether_header) + ip->ip_hl * 4 +
+                                    sizeof(struct udphdr) &&
+               "Truncated UDP datagram.");
+        assert(ip->ip_len >= ip->ip_hl * 4 + sizeof(struct udphdr) &&
+               "Truncated UDP datagram.");
+        struct udphdr *udp = (struct udphdr *)(((char *)ip) + ip->ip_hl * 4);
+        assert(ntohs(udp->len) >= 8 && "Truncated UDP datagram.");
+
+        udp->check = 0;
+      } break;
+      default:
+        fprintf(stderr, "Packet with unsupported transport protocol: %d\n",
+                ip->ip_p);
+      }
+
+      ip->ip_sum = checksum(ip, ip->ip_hl * 4);
+
       pcap_dump((u_char *)out, &pkthdr, o->bytes);
     }
   }
