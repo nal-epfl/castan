a445ddc38f13054ac9ab5afd5ebd902955a6373d
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-10 17:36:45 +0200

    Removed undefined variable.

--- scripts/perf/clean/middlebox.sh
+++ scripts/perf/clean/middlebox.sh
@@ -9,7 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "[clean] Unbinding middlebox interfaces from Linux..."
 sudo ifconfig $MB_DEVICE_INTERNAL down
 sudo ifconfig $MB_DEVICE_EXTERNAL down
-sudo ifconfig $MB_DEVICE_TO_SRV down
 
 echo "[clean] Unbinding middlebox interfaces from DPDK..."
 sudo $RTE_SDK/tools/dpdk-devbind.py -b $KERN_NIC_DRIVER $MB_PCI_INTERNAL $MB_PCI_EXTERNAL || true
