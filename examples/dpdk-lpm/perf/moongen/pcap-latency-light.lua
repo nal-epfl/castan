0eea62b426eb14f7bc59c8ff609f2ca6edfe4bc1
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2017-04-10 13:05:01 +0200

    the pcap-based latency measurement script. warning: it requires certain modifications of very packet in the NF under test, see following commits

--- examples/dpdk-lpm/perf/moongen/pcap-latency-light.lua
+++ examples/dpdk-lpm/perf/moongen/pcap-latency-light.lua
@@ -28,66 +28,93 @@ function master(args)
   printf("heatup  - %d secs", args.upheat);
   local timerTask = mg.startTask("timerSlave", txDev:getTxQueue(0), rxDev:getRxQueue(0), args.upheat, args.file)
   mg.waitForTasks()
-  printf("heatup finished");
+  printf("heatup finished. testing is commented out");
   mg.waitForTasks()
   -- Testing phase
-  local timerTask = mg.startTask("timerSlave", txDev:getTxQueue(0), rxDev:getRxQueue(0), args.timeout, args.file)
-  local latency, stdev = timerTask:wait()
-  printf("total: %f latency (+-%f)", latency, stdev);
-  mg.waitForTasks()
-  file:write(args.rate .. " " .. latency .. " " .. stdev .. "\n")
+  --local timerTask = mg.startTask("timerSlave", txDev:getTxQueue(0), rxDev:getRxQueue(0), args.timeout, args.file)
+  --local latency, stdev = timerTask:wait()
+  --printf("total: %f latency (+-%f)", latency, stdev);
+  --mg.waitForTasks()
+  --file:write(latency .. " " .. stdev .. "\n")
+end
+
+function myMeasureLatency(txQueue, rxQueue, buf, rxBufs) 
+  maxWait = 15/1000
+  --txBufs:alloc(64)
+  --local buf = txBufs[1]
+  buf:enableTimestamps()
+  ts.syncClocks(txQueue.dev, rxQueue.dev)
+  txQueue:sendSingle(buf)
+  local tx = txQueue:getTimestamp(500)
+  local numPkts = 0
+  if tx then
+    local timer = timer:new(maxWait)
+    while timer:running() do
+      local rx = rxQueue:tryRecv(rxBufs, 1000)
+      numPkts = numPkts + rx
+      local timestampedPkt = rxQueue.dev:hasRxTimestamp()
+      if not timestampedPkt then
+        rxBufs:freeAll()
+      else
+        for i = 1, rx do
+          local buf = rxBufs[i]
+          if buf:hasTimestamp() then
+            local rxTs = rxQueue:getTimestamp(nil, nil)
+            if not rxTs then
+              return nil, numPkts
+            end
+            rxBufs:freeAll()
+            local lat = rxTs - tx
+            if 0 < lat and lat < 2 * maxWait * 10^9 then
+              return lat, numPkts
+            end
+          end
+        end
+      end
+    end
+    return nil, numPkts
+  else
+    log:warn("Failed to timestamp packet on transmission")
+    timer:new(maxWait):wait()
+    return nil, numPkts
+  end
 end
 
 function timerSlave(txQueue, rxQueue, duration, fname)
-  txQueue:enableTimestamps()
-  rxQueue:enableTimestamps()
-	local mempool = memory.createMemPool()
-	local finished = timer:new(duration)
-	local hist = hist:new()
+
+  local hist = hist:new()
   local pcapFile = pcap:newReader(fname)
+  local finished = timer:new(duration)
+  txQueue:enableTimestamps()
+  --rxQueue:enableTimestampsAllPackets() -- not supported by current ixgbe driver, and maybe even by the 82599 card
+  -- Important note: the NF must convert every packet into a PTP one, to enable the RX timestamps
+  rxQueue:enableTimestamps()
+  local mempool = memory.createMemPool--()
+  (function(buf)
+        local pkt = buf:getPtpPacket()
+        pkt.eth:fill{ethType=0x88f7}
+	pkt.ptp:setVersion()
+  end) --(function(buf) buf:getPtpPacket():fill() end) ]]
+  local txBufs = mempool:bufArray(1)
   local rxBufs = mempool:bufArray(128)
-	while finished:running() and mg.running() do
-    local buf = pcapFile:readSingle(mempool)
+
+
+  while finished:running() and mg.running() do
+    --[[local buf = pcapFile:readSingle(mempool)
     if buf == nil then
       pcapFile:reset()
-    else
-
-      ts.syncClocks(txQueue.dev, rxQueue.dev)
-      rxQueue.dev:clearTimestamps()
-      -- txQueue.dev:clearTimestamps()
-
-      --buf:offloadUdpChecksums()
-      buf:enableTimestamps()
-      txQueue:sendSingle(buf)
-      local tx = txQueue:getTimestamp(500)
-	log:warn("tx tstmp: " .. tx)
-
-      local rx = rxQueue:tryRecv(rxBufs, 10000);
-      if 0 < rx then
-
-	      if rxQueue.dev:hasRxTimestamp() then
-log:warn("timestamped")
-		local rxBuf = rxBufs[1]
-		if rxBuf:hasTimestamp() then
-		  local rxTs = rxQueue:getTimestamp(nil)
-		  local lat = rxTs - tx
-		  if lat < 0 or 2*10000 < lat then
-		    hist:update(lat)
-		  else
-		    log:warn("Unrealistic latency detected")
-		  end
-		end
-	      else
-		rxBufs:freeAll();
-	      end
-      else
-
-      end
-    end
-	end
-	mg.sleepMillis(300)
-	hist:print()
-	hist:save("latency-histogram.csv")
-	return hist:median(), hist:standardDeviation()
+    else]]
+      txBufs:alloc(64);
+      local buf = txBufs[1]
+      local lat = myMeasureLatency(txQueue, rxQueue, buf, rxBufs)
+      hist:update(lat)
+    --end
+  end
+  hist:print()
+  if hist.numSamples == 0 then
+    log:error("Received no packets.")
+  end
+  print()
+  return hist:median(), hist:standardDeviation()
 end
 
