3484ac7a662a15a23f9023a126253b97eee6efcb
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-10 17:35:18 +0200

    Removed undefined variable.

--- scripts/perf/clean/middlebox.sh
+++ scripts/perf/clean/middlebox.sh
@@ -12,7 +12,7 @@ sudo ifconfig $MB_DEVICE_EXTERNAL down
 sudo ifconfig $MB_DEVICE_TO_SRV down
 
 echo "[clean] Unbinding middlebox interfaces from DPDK..."
-sudo $RTE_SDK/tools/dpdk-devbind.py -b $KERN_NIC_DRIVER $MB_PCI_INTERNAL $MB_PCI_EXTERNAL $MB_PCI_TO_SRV || true
+sudo $RTE_SDK/tools/dpdk-devbind.py -b $KERN_NIC_DRIVER $MB_PCI_INTERNAL $MB_PCI_EXTERNAL || true
 
 echo "[clean] Killing the NF on middlebox..."
 sudo pkill -9 nf | true
