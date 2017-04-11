d8f53401a0b867466d6bca2864a32bdc4b9db0a8
Jonas Fietz <info@jonasfietz.de>
2017-04-11 16:26:21 +0200

    proper uniform distribution across flows

--- tools/fb-to-pcap/create_uniform_distribution_pcap.py
+++ tools/fb-to-pcap/create_uniform_distribution_pcap.py
@@ -30,54 +30,30 @@ class IPTable():
 
 
 
-def create_flows(num_flows, num_bytes, output_file):
+def create_flows(num_flows, num_packets, bytes_per_packet, output_file):
     pktdump = PcapWriter(output_file, append=False)
 
-    first_iteration = True
-    cur_seq = 1002
     ip_table = IPTable()
-    while num_bytes > 0:
-        for f in xrange(num_flows): 
-            src_ip, dst_ip, prot, sport, dport = ip_table.lookup_flow(f)
 
-            pkt = IP(src=src_ip, dst=dst_ip)
+    for i in xrange(num_packets): 
+        f = random.randint(1, num_flows)
+        src_ip, dst_ip, prot, sport, dport = ip_table.lookup_flow(f)
 
-            if prot == 6:
-                # TCP
-                if first_iteration:
-                    # SYN
-                    pkt = pkt/TCP(sport=sport,dport=dport,flags='S', seq=1000)
-                    pktdump.write(pkt)
-                    pkt = IP(src=dst_ip, dst=src_ip)/TCP(sport=dport,dport=sport,flags='SA',seq=1000, ack=1000)
-                    pktdump.write(pkt)
-                    pkt = IP(src=src_ip, dst=dst_ip)/TCP(sport=sport,dport=dport,flags='A',seq=1001,ack=1001)
-                    pktdump.write(pkt)
-                    pkt = IP(src=src_ip, dst=dst_ip)
-                    
-                pkt = pkt/TCP(sport=sport,dport=dport,seq=cur_seq)
+        pkt = IP(src=src_ip, dst=dst_ip)
+        pkt = pkt/UDP(sport=sport,dport=dport)
+        pkt.len = bytes_per_packet
 
-            elif prot == 17:
-                # UDP
-                pkt = pkt/UDP(sport=sport,dport=dport)
-            else:
-                continue
-
-            pkt.len = 1500 if num_bytes > 1500 else num_bytes
-            pktdump.write(pkt)
-
-        num_bytes -= 1500
-        cur_seq += 1500
+        pktdump.write(pkt)
 
 
 if __name__ == "__main__":
     # parse params
     parser = argparse.ArgumentParser(description="create a pcap file with n flows with equal distribution of b bytes")
-    parser.add_argument('--nflows', nargs=1, type=int, help='number of flows')
-    parser.add_argument('--npackets', nargs=1, type=int, help='number of packets total (across all flows)')
-    parser.add_argument('--bytes_per_packet', nargs=1, type=int, help='number of bytes per packet')
-
-    parser.add_argument('--output', nargs=1, help='name of output pcap file')
+    parser.add_argument('--nflows', type=int, help='number of flows', required=True)
+    parser.add_argument('--npackets', type=int, help='number of packets total (across all flows)', required=True)
+    parser.add_argument('--bytes_per_packet',  type=int, help='number of bytes per packet', required=True)
+    parser.add_argument('--output',  help='name of output pcap file', required=True)
 
     args = parser.parse_args()
 
-    create_flows(args.n[0], args.b[0], args.output[0])
+    create_flows(args.nflows, args.npackets, args.bytes_per_packet, args.output)
