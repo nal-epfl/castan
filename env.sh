940ca201aa111358c368bf7f38d7bc6549e5aa0b
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-12 20:17:33 +0200

    Add pcap_tools to PATH.

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export PATH=$PATH:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Debug+Asserts/bin:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Release+Debug+Asserts/bin:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/scripts:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/scripts/perf
+export PATH=$PATH:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Debug+Asserts/bin:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/build/Release+Debug+Asserts/bin:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/scripts:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/scripts/perf:$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)/scripts/pcap_tools
