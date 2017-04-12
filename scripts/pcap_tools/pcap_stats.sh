d8bb25269a5da72ea0b4ca1d7aadcf5054e363b5
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-12 20:47:54 +0200

    Added script to extract basic pcap workload information.

---
+++ scripts/pcap_tools/pcap_stats.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tcpdump -nvr $1 | awk '
+  />/ {
+    flow = $1 ":" $3;
+    flow_cnt[flow]++;
+    packet_cnt++;
+  }
+
+  END {
+    print packet_cnt, "packets."
+    print length(flow_cnt), "flows."
+  }
+'
