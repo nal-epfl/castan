c25fed840a36f4610dcf6b2d045ac9d921f4c064
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2017-04-14 19:51:39 +0200

    Batch benchmarking script

---
+++ scripts/perf/bench-all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/config.sh
+
+SCENARIO=$1
+
+if [ -z $SCENARIO ]; then
+    echo "[bench-all] No scenario specified" 1>&2
+    exit 1
+fi
+
+# Initialize the machines, i.e. software+scripts
+$DIR/setup-machines.sh
+# Clean first, just in case
+$DIR/clean.sh
+$DIR/init-machines.sh
+
+NOW=$(date +"%d.%m.%Y_%H_%M")
+
+MIDDLEBOXES=("dpdk-lpm-dpdklpm") # "dpdk-lpm-btrie" "dpdk-nat-stlmap")
+
+mkdir -p $NOW
+
+for MIDDLEBOX in ${MIDDLEBOXES[@]}; do
+    echo "[bench-all] testing $MIDDLEBOX"
+    $DIR/start-middlebox.sh $MIDDLEBOX
+    while read -u 10 PCAP_FILE; do
+        echo "[bench-all] running pcap file: $PCAP_FILE"
+        CLEAN_APP_NAME=`echo "$MIDDLEBOX" | tr '/' '_'`
+        CLEAN_PCAP_NAME=`echo "$PCAP_FILE" | tr '/' '_'`
+        RESULTS_FILE="bench-$CLEAN_APP_NAME-$SCENARIO-$CLEAN_PCAP_NAME.results"
+        $DIR/run.sh $MIDDLEBOX $SCENARIO $RESULTS_FILE $PCAP_FILE
+    done 10<pcaplist/$MIDDLEBOX.txt
+    $DIR/stop-middlebox.sh
+done
+
+$DIR/clean.sh
