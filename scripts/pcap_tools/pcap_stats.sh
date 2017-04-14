167cb29023a5e28fce2cd2dbd8ff98ff6c77b72c
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-14 16:59:26 +0200

    Calculate average packet length.

--- scripts/pcap_tools/pcap_stats.sh
+++ scripts/pcap_tools/pcap_stats.sh
@@ -7,8 +7,13 @@ tcpdump -nvr $1 | awk '
     packet_cnt++;
   }
 
+  /length [0-9]*)/ {
+    sum_length += $15;
+  }
+
   END {
     print packet_cnt, "packets."
     print length(flow_cnt), "flows."
+    print sum_length / packet_cnt, "avg bytes / pkt."
   }
 '
