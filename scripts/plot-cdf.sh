a80a478225c9a6292a86769e7b954d0423fd53ec
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-15 16:18:02 +0200

    Allow user to set plot range and line titles. Cache computed CDF.

--- scripts/plot-cdf.sh
+++ scripts/plot-cdf.sh
@@ -2,48 +2,55 @@
 
 set -e
 
-
-
 OUTPUT="$1"
-shift
+RANGE="$2"
+shift 2
 
 HISTOGRAM=$(mktemp)
-declare -a CDFS
-
-while (("$#")); do
-  cat $1 | sort -n | uniq -c > $HISTOGRAM
-
-  TOTAL=$(awk '{sum += $1} END {print sum;}' $HISTOGRAM)
-
-  CDFS+=($(mktemp))
-  cat $HISTOGRAM | awk "
-    BEGIN {
-      print \"0,0\";
-    }
-
-    {
-      print \$2 \",\" (acc / $TOTAL);
-      acc += \$1;
-      print \$2 \",\" (acc / $TOTAL);
-    }" > ${CDFS[-1]}
-
-  shift
-done
 
 PLOT_LINES="plot"
-for i in $(seq 0 $((${#CDFS[@]} - 1))); do
-  PLOT_LINES+=" '${CDFS[$i]}' using 1:2 with line ls $i lw 5,"
+while (("$#")); do
+  CSV="$1"
+  TITLE="$2"
+  CDF="$CSV.cdf"
+
+  if [ "$CSV" -nt "$CDF" ]; then
+    echo "Processing $CSV."
+
+    sort -n --parallel=$(grep -c 'processor' /proc/cpuinfo) $CSV \
+        | uniq -c > $HISTOGRAM
+
+    TOTAL=$(awk '{sum += $1} END {print sum;}' $HISTOGRAM)
+
+    CDFS+=($(mktemp))
+    cat $HISTOGRAM | awk "
+      BEGIN {
+        print \"0,0\";
+      }
+
+      {
+        print \$2 \",\" (acc / $TOTAL);
+        acc += \$1;
+        print \$2 \",\" (acc / $TOTAL);
+      }" > $CDF
+  fi
+
+  PLOT_LINES+=" '$CDF' using 1:2 title '$TITLE' with line lw 5,"
+
+  shift 2
 done
 
-gnuplot > $1.eps <<EOF
-set ylabel 'CDF'
-set xlabel 'Latency (ns)'
-set grid
-set key off
-set term epscairo
-set output '$OUTPUT'
-set datafile separator ","
-$PLOT_LINES
+echo "Plotting CDF into $OUTPUT."
+
+gnuplot <<EOF
+  set ylabel 'CDF'
+  set xlabel 'Latency (ns)'
+  set grid
+  set xr [$RANGE]
+  set term epscairo
+  set output '$OUTPUT'
+  set datafile separator ","
+  $PLOT_LINES
 EOF
 
-rm $HISTOGRAM ${CDFS[@]}
+rm $HISTOGRAM
