083e3762a4c890665f5ff74a48c9407ec5a88f95
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-15 12:57:36 +0200

    Plot multiple CDFs.

--- scripts/plot-cdf.sh
+++ scripts/plot-cdf.sh
@@ -2,30 +2,48 @@
 
 set -e
 
+
+
+OUTPUT="$1"
+shift
+
 HISTOGRAM=$(mktemp)
-CDF=$(mktemp)
+declare -a CDFS
 
-cat $1 | sort -n | uniq -c > $HISTOGRAM
+while (("$#")); do
+  cat $1 | sort -n | uniq -c > $HISTOGRAM
 
-TOTAL=$(awk '{sum += $1} END {print sum;}' $HISTOGRAM)
+  TOTAL=$(awk '{sum += $1} END {print sum;}' $HISTOGRAM)
 
-cat $HISTOGRAM | awk "
-  BEGIN {
-    print \"0,0\";
-  }
+  CDFS+=($(mktemp))
+  cat $HISTOGRAM | awk "
+    BEGIN {
+      print \"0,0\";
+    }
 
-  {
-    acc += \$1;
-    print \$2 \",\" (acc / $TOTAL);
-  }" > $CDF
+    {
+      print \$2 \",\" (acc / $TOTAL);
+      acc += \$1;
+      print \$2 \",\" (acc / $TOTAL);
+    }" > ${CDFS[-1]}
+
+  shift
+done
+
+PLOT_LINES="plot"
+for i in $(seq 0 $((${#CDFS[@]} - 1))); do
+  PLOT_LINES+=" '${CDFS[$i]}' using 1:2 with line ls $i lw 5,"
+done
 
 gnuplot > $1.eps <<EOF
 set ylabel 'CDF'
 set xlabel 'Latency (ns)'
 set grid
-set term eps
-set output '$1.eps'
-plot '$CDF' with line lt -1 lw 2
+set key off
+set term epscairo
+set output '$OUTPUT'
+set datafile separator ","
+$PLOT_LINES
 EOF
 
-rm $HISTOGRAM $CDF
+rm $HISTOGRAM ${CDFS[@]}
