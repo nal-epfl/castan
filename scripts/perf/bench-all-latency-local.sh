a3372cb335f986db03aba77d313bdbf9e018c31d
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-16 21:15:28 +0200

    Don't rerun experiemnts with existing results.

--- scripts/perf/bench-all-latency-local.sh
+++ scripts/perf/bench-all-latency-local.sh
@@ -7,10 +7,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for NF in dpdk-lpm-btrie \
           dpdk-lpm-dpdklpm; do
   [ -f "~/pcap/castan/$NF-castan.pcap" ] \
+      && [ ! -f "~/results/$NF-$NF-castan.log" ] \
       && $DIR/bench-latency-local.sh $NF ~/pcap/castan/$NF-castan.pcap \
              --pfx2as ~/castan/scripts/perf/routing-table.pfx2as
 
   [ -f "~/pcap/random/$NF-unirand.pcap" ] \
+      && [ ! -f "~/results/$NF-$NF-unirand.log" ] \
       && $DIR/bench-latency-local.sh $NF ~/pcap/random/$NF-unirand.pcap \
              --pfx2as ~/castan/scripts/perf/routing-table.pfx2as
 
@@ -22,6 +24,7 @@ for NF in dpdk-lpm-btrie \
               ~/pcap/random/imc10-2pt8-zipf.pcap \
               ~/pcap/random/unirand1000.pcap; do
     [ -f "$PCAP" ] \
+        && [ ! -f "~/results/$NF-$(echo $PCAP | sed -e 's#.*/\(.*\)\.pcap#\1#').log" ] \
         && $DIR/bench-latency-local.sh $NF $PCAP \
                --pfx2as ~/castan/scripts/perf/routing-table.pfx2as
   done
@@ -33,10 +36,12 @@ for NF in dpdk-nat-basichash \
           dpdk-nat-stlmap \
           dpdk-nat-stlumap; do
   [ -f "~/pcap/castan/$NF-castan.pcap" ] \
+      && [ ! -f "~/results/$NF-$NF-castan.log" ] \
       && $DIR/bench-latency-local.sh $NF ~/pcap/castan/$NF-castan.pcap \
              --nat-ip 192.168.0.1
 
   [ -f "~/pcap/random/$NF-unirand.pcap" ] \
+      && [ ! -f "~/results/$NF-$NF-unirand.log" ] \
       && $DIR/bench-latency-local.sh $NF ~/pcap/random/$NF-unirand.pcap \
              --nat-ip 192.168.0.1
 
@@ -48,6 +53,7 @@ for NF in dpdk-nat-basichash \
               ~/pcap/random/imc10-2pt8-zipf.pcap \
               ~/pcap/random/unirand1000.pcap; do
     [ -f "$PCAP" ] \
+        && [ ! -f "~/results/$NF-$(echo $PCAP | sed -e 's#.*/\(.*\)\.pcap#\1#').log" ] \
         && $DIR/bench-latency-local.sh $NF $PCAP \
                --nat-ip 192.168.0.1
   done
@@ -61,6 +67,7 @@ for PCAP in ~/pcap/facebook/cluster_A_1.pcap \
             ~/pcap/random/imc10-2pt8-zipf.pcap \
             ~/pcap/random/unirand1000.pcap; do
   [ -f "$PCAP" ] \
+      && [ ! -f "~/results/dpdk-nat-cc-$(echo $PCAP | sed -e 's#.*/\(.*\)\.pcap#\1#').log" ] \
       && $DIR/bench-latency-local.sh dpdk-nat-cc $PCAP \
-              --extip 192.168.0.1 --expire 120 --max-flows 65536 --wan 1
+             --extip 192.168.0.1 --expire 120 --max-flows 65536 --wan 1
 done
