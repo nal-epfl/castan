100e0320aae80d4777b5a8d22c8157805bd0985d
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-04-16 20:06:13 +0200

    Background the NF and kill it after 2s.

--- scripts/perf/bench-latency-local.sh
+++ scripts/perf/bench-latency-local.sh
@@ -18,4 +18,9 @@ sudo rm -rf build
 make clean
 make
 
-sudo ./build/nf --vdev "eth_pcap0,rx_pcap=$PCAP,tx_pcap=/dev/null" --vdev 'eth_pcap1,rx_pcap=/home/lpedrosa/pcap/empty.pcap,tx_pcap=/dev/null' -- --eth-dest 0,90:e2:ba:55:12:25 --eth-dest 1,90:e2:ba:55:12:24 $@ 2>&1 ~/results/$NF-$CLEAN_PCAP.log
+sudo ./build/nf --vdev "eth_pcap0,rx_pcap=$PCAP,tx_pcap=/dev/null" --vdev 'eth_pcap1,rx_pcap=/home/lpedrosa/pcap/empty.pcap,tx_pcap=/dev/null' -- --eth-dest 0,90:e2:ba:55:12:25 --eth-dest 1,90:e2:ba:55:12:24 $@ 2>&1 > ~/results/$NF-$CLEAN_PCAP.log &
+PID=$!
+
+wait 2
+
+kill -2 $PID
