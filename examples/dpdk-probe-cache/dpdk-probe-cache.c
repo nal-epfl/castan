1f8be1442da8eef0743df83d0ee408d0041a1364
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2017-10-04 12:51:23 +0200

    Fixed args.

--- examples/dpdk-probe-cache/dpdk-probe-cache.c
+++ examples/dpdk-probe-cache/dpdk-probe-cache.c
@@ -201,13 +201,13 @@ int main(int argc, char *argv[]) {
   argc -= ret;
   argv += ret;
 
-  assert(argc == 1 && "Usage: probe-slices <set-file>");
-  FILE *file = fopen(argv[0], "w");
+  assert(argc == 2 && "Usage: probe-slices <set-file>");
+  FILE *file = fopen(argv[1], "w");
   assert(file && "Unable to open set file.");
 
   printf("Exploring array of %ld bytes, %ld lines.\n", ARRAY_SIZE,
          ARRAY_SIZE >> OFFSET_BITS);
-  printf("Writing contention sets to: %s.\n", argv[0]);
+  printf("Writing contention sets to: %s.\n", argv[1]);
 
   const struct rte_memzone *mz = rte_memzone_reserve_aligned(
       "Array", ARRAY_SIZE, rte_socket_id(), RTE_MEMZONE_1GB, PAGE_SIZE);
