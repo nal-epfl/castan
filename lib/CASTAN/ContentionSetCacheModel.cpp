a3f7bafb96adb336ab3af222d95f1d83c437456a
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-01-10 13:53:38 +0100

    Fixed hit checking.

--- lib/CASTAN/ContentionSetCacheModel.cpp
+++ lib/CASTAN/ContentionSetCacheModel.cpp
@@ -242,6 +242,18 @@ klee::ref<klee::Expr> ContentionSetCacheModel::memoryOperation(
         for (long a : contentionSets[set.second].first) {
           klee::klee_message("    Trying address %08lX.", a);
 
+          bool hit = false;
+          for (auto idx : contentionSetIdxs[a]) {
+            if (cache[idx].count(a)) {
+              hit = true;
+              break;
+            }
+          }
+          if (hit) {
+            klee::klee_message("      Already a hit.");
+            continue;
+          }
+
           // Constrain cache line:
           // a == address & ((1<<PAGE_BITS)-1) & ~((1<<BLOCK_BITS)-1)
           klee::ConstraintManager constraints(state.constraints);
@@ -266,18 +278,6 @@ klee::ref<klee::Expr> ContentionSetCacheModel::memoryOperation(
           if (executor->solver->solver->getValue(klee::Query(constraints, address),
                                        concreteAddress)) {
             //               klee::klee_message("Line fits constraints.");
-
-            bool hit = false;
-            for (auto idx : contentionSetIdxs[a]) {
-              if (cache[idx].count(concreteAddress->getZExtValue())) {
-                hit = true;
-                break;
-              }
-            }
-            if (hit) {
-              klee::klee_message("      Already a hit.");
-              continue;
-            }
 
             hitCount++;
             klee::klee_message("    Found potential hit, %d more needed.",
