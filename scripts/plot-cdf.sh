083065b5e1086325e48c102a6d9d2cf23d969f45
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2018-02-01 05:28:10 +0100

    Adjust margins for the plots

--- scripts/plot-cdf.sh
+++ scripts/plot-cdf.sh
@@ -15,9 +15,9 @@ GLOBAL_MAX=-9223372036854775808 # INT_MIN
 
 # Line style parameters
 declare -A LINE_STYLES
-LINE_STYLES["nop"]='dt 1 lw 5 lc rgb "#000000"'
+LINE_STYLES["nop"]='dt 1 lw 3 lc rgb "#000000"'
 LINE_STYLES["1packet"]='dt 2 lw 7 lc rgb "#E69F00"'
-LINE_STYLES["zipf"]='dt 3 lw 8 lc rgb "#56B4E9"'
+LINE_STYLES["zipf"]='dt 7 lw 3 lc rgb "#56B4E9"'
 LINE_STYLES["unirand-all"]='dt 4 lw 4 lc rgb "#009E73"'
 LINE_STYLES["unirand-castan"]='dt 5 lw 5 lc rgb "#F0E442"'
 LINE_STYLES["castan"]='dt 6 lw 6 lc rgb "#0072B2"'
@@ -93,7 +93,11 @@ fi
 echo "Plotting CDF into $OUTPUT."
 
 gnuplot <<EOF
-  set ylabel 'CDF'
+  set bmargin 3.5
+  set lmargin 5.5
+  set rmargin 5.5
+  set tmargin 0.5
+  set ylabel 'CDF' offset 2.3,0
   set xlabel '$XLABEL'
   set style line 12 lc rgb '#b0b0b0' lt 0 lw 1
   set grid back ls 12
