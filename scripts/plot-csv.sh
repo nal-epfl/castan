9c7220fd9f048cf85da290017841b083b93fb68f
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-07-25 18:15:50 +0200

    Plot simple sequences. Escape _ in titles. Added options for log scale.

--- scripts/plot-csv.sh
+++ scripts/plot-csv.sh
@@ -9,7 +9,12 @@ PLOT_LINES="plot"
 while (("$#")); do
   CSV="$1"
 
-  PLOT_LINES+=" '$CSV' using 1:2 title '$(echo $CSV | sed s/\.csv//)' with line lw 5,"
+  if ! (head -n 1 $CSV | grep ',' >/dev/null 2>/dev/null); then
+    awk 'BEGIN {c = 0} {print c++ "," $0}' $CSV > $CSV.sequence.csv
+    CSV="$CSV.sequence.csv"
+  fi
+
+  PLOT_LINES+=" '$CSV' using 1:2 title '$(echo $CSV | sed -e s/\.csv$// -e s/_/\\\\_/g)' with line lw 5,"
 
   shift 1
 done
@@ -21,5 +26,7 @@ gnuplot <<EOF
   set term epscairo
   set output '$OUTPUT'
   set datafile separator ","
+#   set logscale y
+#   set format y "10^{%L}"
   $PLOT_LINES
 EOF
